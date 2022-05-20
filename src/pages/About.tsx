63af6c6847a6eb5456a931139baa04da96fbdddc
Idris Akintobi Adejare <akintobiidris@gmail.com>
2022-05-20 02:30:06 +0100

    Created About page

---
+++ src/pages/About.tsx
@@ -0,0 +1,13 @@
+function About() {
+  return (
+    <div className="ml-4">
+      <h1 className="text-6xl mb-3">GitHub Finder</h1>
+      <p className="mb-3 text-2xl font-light">
+        A react App to search for GitHub profile and display the profile
+        information's
+      </p>
+    </div>
+  );
+}
+
+export default About;
