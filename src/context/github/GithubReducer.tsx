ae44b257966d1198119a6229a22c8aa13d5c298a
Idris Akintobi Adejare <akintobiidris@gmail.com>
2022-05-20 02:32:10 +0100

    Created reducer

---
+++ src/context/github/GithubReducer.tsx
@@ -0,0 +1,49 @@
+type StateType = {
+  users: [];
+  user: {};
+  repos: {};
+  loading: boolean;
+};
+
+type ActionType = {
+  type: string;
+  payload?: any;
+};
+
+const GithubReducer = (state: StateType, action: ActionType) => {
+  switch (action.type) {
+    case "GET_USERS":
+      return {
+        ...state,
+        users: action.payload,
+        loading: false,
+      };
+    case "GET_USER":
+      return {
+        ...state,
+        user: action.payload,
+        loading: false,
+      };
+    case "SET_LOADER":
+      return {
+        ...state,
+        loading: true,
+      };
+    case "GET_REPO":
+      return {
+        ...state,
+        repos: action.payload,
+        loading: false,
+      };
+    case "CLEAR_USER":
+      return {
+        ...state,
+        users: [],
+        loading: false,
+      };
+    default:
+      return state;
+  }
+};
+
+export default GithubReducer;
